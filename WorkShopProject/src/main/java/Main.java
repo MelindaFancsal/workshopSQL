b01d1c57f7438d84bf6f9111b8b1321ca142c20c
Melinda Fancsal <melinda.fancsal@gmail.com>
2024-04-24 20:50:50 +0300

    update code

--- WorkShopProject/src/main/java/Main.java
+++ WorkShopProject/src/main/java/Main.java
@@ -1,17 +1,39 @@
 import dao.UsersDAO;
 import objects.User;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         UsersDAO usersDAO = new UsersDAO();
 
-        User user = new User();
-        user.setUserName("Andreea");
-        user.setEmail("andreea@andreea.ro");
-        user.setPassword("ljlkjlkj");
+        callCreateUser(usersDAO);
+        User user = callReadUser(usersDAO);
 
-       User updatedUser =  usersDAO.create(user);
+        //update user
+//        user.setEmail("andreea_new_email@andreea.ro");
+//        usersDAO.update(user);
+
+        User[] arrayUsers = usersDAO.readAll();
+        System.out.println(Arrays.toString(arrayUsers));
+    }
+
+    private static User callReadUser(UsersDAO usersDAO) {
+        User result = usersDAO.read(1);
+        System.out.println(result);
+
+        return result;
+    }
+
+    private static void callCreateUser(UsersDAO usersDAO) {
+        User user = new User();
+        user.setUserName("Jeni");
+        user.setEmail("jeni@andreea.ro");
+        user.setPassword("iokhjkh");
+
+        User updatedUser = usersDAO.create(user);
 
         System.out.println(updatedUser);
     }
--- WorkShopProject/src/main/java/dao/UsersDAO.java
+++ WorkShopProject/src/main/java/dao/UsersDAO.java
@@ -5,6 +5,7 @@ import objects.User;
 import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.*;
+import java.util.Arrays;
 
 public class UsersDAO {
     private static final String CREATE_USER_QUERY =
@@ -19,6 +20,7 @@ public class UsersDAO {
         try (Connection conn = DBUtil.getConnection()) {
             PreparedStatement statement =
                     conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+
             statement.setString(1, user.getUserName());
             statement.setString(2, user.getEmail());
             statement.setString(3, hashPassword(user.getPassword()));
@@ -28,6 +30,7 @@ public class UsersDAO {
             if (resultSet.next()) {
                 user.setId(resultSet.getInt(1));
             }
+
             return user;
         } catch (SQLException e) {
             e.printStackTrace();
@@ -35,9 +38,95 @@ public class UsersDAO {
         }
     }
 
+
+    public User read(int userId) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(READ_USER_QUERY);
+            statement.setInt(1, userId);
+
+            User user = new User();
+            ResultSet rs = statement.executeQuery();
+
+            if (rs.next()) {
+                int id = rs.getInt("id");
+
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                String email = rs.getString("email");
+
+                user.setId(id);
+                user.setUserName(username);
+                user.setPassword(password);
+                user.setEmail(email);
+            }
+
+            return user;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update(User user) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(UPDATE_USER);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getPassword());
+            statement.setInt(4, user.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void delete(int userId) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(DELETE_USER);
+            statement.setInt(1, userId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public User[] readAll() {
+        try (Connection conn = DBUtil.getConnection()) {
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery(READ_USERS_QUERY);
+
+            User[] userArray = new User[]{};
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String email = rs.getString("email");
+                String password = rs.getString("password");
+                String username = rs.getString("username");
+
+                User user = new User();
+                user.setEmail(email);
+                user.setUserName(username);
+                user.setId(id);
+                user.setPassword(password);
+
+                userArray = addToArray(user, userArray);
+            }
+
+            return userArray;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private String hashPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
 
+    private User[] addToArray(User u, User[] users) {
+        User[] tmpUsers = Arrays.copyOf(users, users.length + 1); // Creates a table copy increased by 1.
+        tmpUsers[users.length] = u; // Adds object in the last position.
+        return tmpUsers; // Returns new table.
+    }
 
 }
